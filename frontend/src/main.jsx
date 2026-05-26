6c513ab61db9aab37c76b999cddd55993c4c937d
richardbrownmiami-commits <richard.brown.miami@gmail.com>
2026-05-26 09:43:47 +0530

    update frontend/src/main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import App from "./App"
-import "./index.css"
+const { StrictMode } = React
+const { createRoot } = ReactDOM
 
-createRoot(document.getElementById("root")).render(<StrictMode><App /></StrictMode>)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
